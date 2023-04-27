c36a3773771ac7a600529f2b66d212f8037c14c7
Md Rijon Hossain Jibon <arntoken.tem@gmail.com>
2023-04-27 13:50:35 +0000

    updated

--- src/screen/Login/index.js
+++ src/screen/Login/index.js
@@ -1,4 +1,44 @@
 //import "antd/dist/antd.min.css";
 import { Button, Input } from "antd";
+import { DownloadOutlined,GlobalOutlined,DownOutlined} from '@ant-design/icons'
 import "./LoginScreen.css";
-export const LogInScreen = () => {};
+const Manuarr = ['Buy Credit Card','MARKET','Trading','Earn','Airdrop']
+export const LogInScreen = () => {
+   
+
+
+    return(
+        <>
+        <div className="header">
+           <div className="header-logo-div">
+               LOGO
+           </div>
+           <ul className="header-ul">
+              
+               <li>Buy Crypto  <DownOutlined /></li>
+               <li>MARKET </li>
+               <li>Trading  <DownOutlined /></li>
+               <li>Earn <DownOutlined /></li>
+               
+           </ul>
+           <div className="right-login--reg">
+               <div className="Login">Login</div>
+               <div className="Register">Register</div>
+               <DownloadOutlined className="d-icons"/>
+              <GlobalOutlined  className="g-icons"/>
+           </div>
+           <div className="popup">wdrw</div>
+           
+        </div>
+        
+        <div className="home-page">
+           <div className="ex-name"> Hotcoin Global </div>
+           <div className="ex-name1">The World's Leading Blockchain Digital Asset Trading Platform</div>
+           <div className="ex-name2"> Hassle-free Trading among BTC, ETH, USDT and Other Cryptos</div>
+       
+       
+       
+        </div>
+        </>
+    )
+};
