97ad6840175adbd477b87f346564c2e6d18e0345
Md Rijon Hossain Jibon <arntoken.tem@gmail.com>
2023-04-27 04:30:52 +0000

    CODE UPDATE

---
+++ src/components/header/index.js
@@ -0,0 +1,7 @@
+export const WEBHEADER = () => {
+  return (
+    <div className="header">
+      <div class="header-logo-div">logo</div>
+    </div>
+  );
+};
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,5 +2,6 @@ import { CandelChart } from "./Chart";
 import { PriceHader } from "./PriceHader";
 import { Spotorderbook } from "./spotorderbook";
 import { TabBuySell } from "./TabBuySell";
+import { WEBHEADER } from './header';
 
-export const  Components = { Spotorderbook,CandelChart,PriceHader ,TabBuySell}
+export const  Components = { Spotorderbook,CandelChart,PriceHader ,TabBuySell,WEBHEADER}
