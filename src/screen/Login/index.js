2b4e504cc8263217f5f4d85d2aef5fd4e79c4eef
Md Rijon Hossain Jibon <arntoken.tem@gmail.com>
2023-04-27 20:58:35 +0000

    css

--- src/screen/Login/index.js
+++ src/screen/Login/index.js
@@ -1,107 +1,104 @@
 //import "antd/dist/antd.min.css";
-import { Button, Input, Carousel,notification ,Table  } from "antd";
-import { DownloadOutlined,GlobalOutlined,DownOutlined,SoundOutlined} from '@ant-design/icons'
+import { Button, Input, Carousel, notification, Table, Space } from "antd";
+import {
+  DownloadOutlined,
+  GlobalOutlined,
+  DownOutlined,
+  SoundOutlined,
+} from "@ant-design/icons";
+
 import "./LoginScreen.css";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-const Manuarr = ['Buy Credit Card','MARKET','Trading','Earn','Airdrop'];
+import { useState } from "react";
+import { Components } from "../../components/";
 
-import { Components } from '../../components/'
-
-
-const dataSource = [
-  {
-    key: '1',
-    market: 'Binance',
-    coinLogo: 'https://www.binance.com/favicon.ico',
-    pair: 'BTC/USDT',
-  },
-  {
-    key: '2',
-    market: 'Coinbase',
-    coinLogo: 'https://www.coinbase.com/favicon.ico',
-    pair: 'ETH/USD',
-  },
-  {
-    key: '3',
-    market: 'Kraken',
-    coinLogo: 'https://www.kraken.com/favicon.ico',
-    pair: 'LTC/EUR',
-  },
-];
-
-const columns = [
-  {
-    title: 'Market',
-    dataIndex: 'market',
-    key: 'market',
-  },
-  {
-    title: 'Coins Logo',
-    dataIndex: 'coinLogo',
-    key: 'coinLogo',
-    render: (logo) => <img src={logo} alt="coin logo" />,
-  },
-  {
-    title: 'Pair',
-    dataIndex: 'pair',
-    key: 'pair',
-  },
-];
+const Manuarr = ["Buy Credit Card", "MARKET", "Trading", "Earn", "Airdrop"];
 
 export const LogInScreen = () => {
- 
+  const [isOpen, setIsOpen] = useState(false);
 
-    return(
-        <>
-        <div className="header">
-           <div className="header-logo-div">
-               LOGO
-           </div>
-           <ul className="header-ul">
-              
-               <li>Buy Crypto  <DownOutlined /></li>
-               <li>MARKET </li>
-               <li>Trading  <DownOutlined /></li>
-               <li>Earn <DownOutlined /></li>
-               
-           </ul>
-           <div className="right-login--reg">
-               <div className="Login">Login</div>
-               <div className="Register">Register</div>
-               <DownloadOutlined className="d-icons"/>
-              <GlobalOutlined  className="g-icons"/>
-           </div>
-           <div className="popup">wdrw</div>
-           
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const menuItems = ["Home", "About", "Services", "Contact"];
+  return (
+    <>
+      <div className="header">
+        <div className="header-logo-div">LOGO</div>
+        <ul className="header-ul">
+          <li>
+            Buy Crypto <DownOutlined  onClick={toggleMenu}/>
+          </li>
+          <li>MARKET </li>
+          <li>
+            Trading <DownOutlined  onClick={toggleMenu}/>
+          </li>
+          <li>
+            Earn <DownOutlined  onClick={toggleMenu}/>
+          </li>
+        </ul>
+        <div className="right-login--reg">
+          <div className="Login">Login</div>
+          <div className="Register">Register</div>
+          <DownloadOutlined className="d-icons" />
+          <GlobalOutlined className="g-icons" />
         </div>
-        
-        <div className="home-page">
-            <div  className="loading-page-title">
-                <div className="ex-name"> DEMO Global </div>
-                 <div className="ex-name1">The World's Leading Blockchain Digital Asset Trading Platform</div>
-                 <div className="ex-name2"> Hassle-free Trading among BTC, ETH, USDT and Other Cryptos</div>
-            </div>
-           
-           
-          
-           
-       
-       <div className="loading-page-rg">
-           <Button className="login-btn" name="Login" title="Login">Login</Button>
-       <Button className="rg-btn" name="Login" title="Register">Register</Button>
-       </div>
-       
-      
-    <Components.WEBSlider/>
-       
-      <div className="banner-not">
-          <div className="news">  <SoundOutlined />  <div style={ { marginLeft : '15px',fontSize : '14px' } }>TEXTTT</div> </div>
+        <div className="popup">wdrw</div>
       </div>
-       
-      <Table dataSource={dataSource} columns={columns} />
+
+      <div className="home-page">
+        <div className="loading-page-title">
+          <div className="ex-name"> DEMO Global </div>
+          <div className="ex-name1">
+            The World's Leading Blockchain Digital Asset Trading Platform
+          </div>
+          <div className="ex-name2">
+            {" "}
+            Hassle-free Trading among BTC, ETH, USDT and Other Cryptos
+          </div>
         </div>
-        </>
-    )
+
+        <div className="loading-page-rg">
+          <Button className="login-btn" name="Login" title="Login">
+            Login
+          </Button>
+          <Button className="rg-btn" name="Login" title="Register">
+            Register
+          </Button>
+        </div>
+
+        <Components.WEBSlider />
+
+        <div className="banner-not">
+          <div className="news">
+            {" "}
+            <SoundOutlined />{" "}
+            <div style={{ marginLeft: "15px", fontSize: "14px" }}>
+              TEXTTT
+            </div>{" "}
+          </div>
+        </div>
+
+        <div className="dropdown-menu">
+          <button className="dropdown-menu__toggle" >
+            Menu
+          </button>
+          <ul
+            className={`dropdown-menu__list ${
+              isOpen ? "dropdown-menu__list--open" : ""
+            }`}
+          >
+            {menuItems.map((item) => (
+              <li key={item} className="dropdown-menu__item">
+                <a href={`/${item.toLowerCase()}`}>{item}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 };
